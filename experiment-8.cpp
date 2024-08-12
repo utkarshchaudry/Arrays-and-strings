5a7e5964261fc5116782fcbd421f84046402a30f
utkarsh chaudhary <chaudryutkarsh@gmail.com>
2024-08-12 15:33:56 +0530

    Update experiment-8.cpp

--- experiment-8.cpp
+++ experiment-8.cpp
@@ -1,1 +1,23 @@
 //name: utkarsh chaudhary
+//PRN: 23070123146
+//EXP-8
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  int temp[3][3], i, j, k, l;
+for (i=0;i<3;i++){
+    for(j=0;j<3;j++){
+        cout<<"enter element-("<<i<<j<<"): ";
+        cin>>temp[i][j];
+    }
+}
+for(k=0;k<3;k++) {
+    for(l=0;l<3;l++ {
+        cout<<temp[k][1];
+        cout<<"  ";
+    }
+    cout<<end1;
+}
+}
